208510da5ee3951b9d85152adaabe11ecc50ff02
borgified <borgified@gmail.com>
2019-04-26 19:59:03 +0000

    a

---
+++ trigger_prep.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+# needs $GITHUB_TOKEN set
+
+BRANCH=master
+REPO_SLUG=borgified%2FBlinkingLabel
+
+travis login --org --github-token $GITHUB_TOKEN
+TRAVIS_TOKEN=$(travis token --no-interactive)
+
+body='{
+ "request": {
+ "message": "Manually triggered prep stage.",
+ "branch": "'${BRANCH}'",
+ "config": {
+   "merge_mode": "deep_merge",
+   "env": {
+     "HUB": "true",
+     "VAR1": "hi",
+     "VAR2": "hello"
+   }
+  }
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $TRAVIS_TOKEN" \
+   -d "$body" \
+   https://api.travis-ci.org/repo/${REPO_SLUG}/requests
